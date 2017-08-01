d5fe4557a301424972ac4f4da2483df693410cf7
artur_rosa <artur_rosa@epam.com>
2017-08-01 17:03:00 +0200

    Logger and I18NResolver

---
+++ src/main/java/com/java_academy/logic/tools/I18NResolver.java
@@ -0,0 +1,33 @@
+package com.java_academy.logic.tools;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18NResolver {
+
+    private static I18NResolver instance;
+    private Locale locale;
+
+    private I18NResolver(Locale locale) {
+        this.locale = locale;
+    }
+
+    static void updateLocale(Locale locale) {
+        getI18NResolverInstance().locale = locale;
+    }
+
+    public static I18NResolver createI18NResolverInstance(Locale locale) {
+        if (instance == null){
+            instance = new I18NResolver(locale);
+        }
+        return instance;
+    }
+    
+    public static I18NResolver getI18NResolverInstance() {
+    	return instance;
+    }
+
+    public static String getMsgByKey(String key) {
+        return ResourceBundle.getBundle("Messages", getI18NResolverInstance().locale).getString(key);
+    }
+}
