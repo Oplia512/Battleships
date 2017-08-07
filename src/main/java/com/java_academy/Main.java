23c7504e5c0073c266a916e7134c442460f88e49
Bartlomiej Janik <bartlomiej9519@gmail.com>
2017-08-07 18:21:16 +0200

    Refactoring welcome window

--- src/main/java/com/java_academy/Main.java
+++ src/main/java/com/java_academy/Main.java
@@ -2,13 +2,22 @@ package com.java_academy;
 
 
 import com.java_academy.gui.Controller;
+import com.java_academy.logic.Game;
 import com.java_academy.logic.model.MessageObject;
+import com.java_academy.logic.tools.BSLog;
 import com.java_academy.network.Connector;
+import com.java_academy.network.socket_provider.ServerSocketProvider;
+import com.java_academy.network.socket_provider.core.SocketProvider;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 
 public class Main extends Application {
 
@@ -24,4 +33,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
